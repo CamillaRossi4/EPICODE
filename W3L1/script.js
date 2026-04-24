e7532854c094d1f197522049c7e05856763f959b
Camilla Rossi <camillarossi.1092@gmail.com>
2026-04-24 18:24:11 +0200

    Adds exercises

---
+++ W3L1/script.js
@@ -0,0 +1,20 @@
+/* DOMANDA 1
+Cos'è un algoritmo? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica.
+*/
+/* 
+Un algoritmpo è una serie di passaggi da seguire per ottenere un risultato, proprio come una ricetta per preparare una torta.
+*/
+
+/* DOMANDA 2
+Cos'è una variabile? Prova a spiegarlo con parole tue, come se lo dovessi spiegare a qualcuno che non ne capisce niente di informatica.
+*/
+/* 
+Una variabile è come un contenitore in una ricetta, serce per conservare gli ingredienti o i risultati che utilizziamo durante i vari passaggi. 
+*/
+
+/* DOMANDA 3
+undefined e null sono la stessa cosa?
+*/
+/* 
+Non sono la stessa cosa. Undefined significa "non ha ancora valore", null significa "è vuoto di proposito".
+*/
